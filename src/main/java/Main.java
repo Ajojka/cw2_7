6b5e50500a6eab16a95a7fb7737bfb6303d6843b
Oliwia <138303743+Ajojka@users.noreply.github.com>
2025-10-27 19:52:48 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Liczby musza byc parzyste, rozne od zera i w zakresie <-10;10>");
+
+        System.out.print("Podaj pierwsza liczbe calkowita: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Podaj druga liczba calkowita: ");
+        int b = scanner.nextInt();
+
+        if (!czyPoprawna(a) || !czyPoprawna(b)) {
+            System.out.println("Blad wprowadzenia liczb");
+        } else {
+            if (a > b) {
+                System.out.println("Wieksza liczba to: " + a);
+            } else if (b > a) {
+                System.out.println("Wieksza liczba to: " + b);
+            } else {
+                System.out.println("Liczby sa rowne.");
+            }
+        }
+
+        scanner.close();
+    }
+
+    public static boolean czyPoprawna(int n) {
+        return n != 0 && n % 2 == 0 && n >= -10 && n <= 10;
+    }
+}
